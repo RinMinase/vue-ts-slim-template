f8cde3cf8cc433fcb9f4db6f6cafd74f91b892ce
Rin Minase <rin.black.raison.detre@gmail.com>
2020-06-16 18:59:53 +0800

    Modified structure to use modules

---
+++ src/views/about/about.vue
@@ -0,0 +1,9 @@
+<script lang="ts" src="./about.ts"></script>
+<style lang="scss" scoped src="./about.scss"></style>
+
+<template>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<button @click="testFn">Click!</button>
+	</div>
+</template>
